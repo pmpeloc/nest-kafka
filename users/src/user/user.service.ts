1fd33df12ed75a6ccecfbca4e535e653c3edb6cf
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-03-13 16:50:40 -0300

    feat: User Seeder added

--- users/src/user/user.service.ts
+++ users/src/user/user.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from './user';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @Inject(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async save(options) {
+    return this.userRepository.save(options);
+  }
+}
